475e51de707af21c5bd3c2170392e7d68bf91fcb
Natalia Galan <n8galan@gmail.com>
2020-06-22 19:19:45 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,5 +1,5 @@
-#Open up lib/tic_tac_toe.rb. You'll be defining the main game class, 
-#TicTacToe in lib/tic_tac_toe.rb. Without that file defining 
+#Open up lib/tic_tac_toe.rb. You'll be defining the main game class,
+#TicTacToe in lib/tic_tac_toe.rb. Without that file defining
 #a TicTacToe class, everything will break.
 #Every method you build will be encapsulated by this class.
 
