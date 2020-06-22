22608c331a5d1b7051ed946dd24cb8660c3861b9
Natalia Galan <n8galan@gmail.com>
2020-06-22 20:06:32 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -31,7 +31,7 @@ WIN_COMBINATIONS = [
   end
 
   def position_taken?(board, index)
-    if @board[index] == " " || @board[index] == "" || @board[index] == nil 
+    if @board[index] == " " || @board[index] == "" || @board[index] == nil
         taken = false
       elsif @board[index] == "X" || @board[index] == "O"
         taken = true
